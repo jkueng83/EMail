8b125d22bc78382caabd8d01d57d2e3cdb93e1b0
jkueng83 <johannes.kueng@gmx.at>
2020-12-05 13:59:54 +0100

    first working version

--- Mail/Form1.Designer.cs
+++ Mail/Form1.Designer.cs
@@ -36,13 +36,19 @@ namespace Mail
             this.lBPassword = new System.Windows.Forms.Label();
             this.lBTo = new System.Windows.Forms.Label();
             this.tBoxTo = new System.Windows.Forms.TextBox();
+            this.lbSubject = new System.Windows.Forms.Label();
+            this.tBoxSubject = new System.Windows.Forms.TextBox();
+            this.lbBody = new System.Windows.Forms.Label();
+            this.tBoxBody = new System.Windows.Forms.TextBox();
+            this.btAddFile = new System.Windows.Forms.Button();
+            this.tBoxAttachment = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnSendMail
             // 
-            this.btnSendMail.Location = new System.Drawing.Point(71, 43);
+            this.btnSendMail.Location = new System.Drawing.Point(112, 379);
             this.btnSendMail.Name = "btnSendMail";
-            this.btnSendMail.Size = new System.Drawing.Size(75, 23);
+            this.btnSendMail.Size = new System.Drawing.Size(445, 23);
             this.btnSendMail.TabIndex = 0;
             this.btnSendMail.Text = "send mail";
             this.btnSendMail.UseVisualStyleBackColor = true;
@@ -50,24 +56,24 @@ namespace Mail
             // 
             // tBoxFrom
             // 
-            this.tBoxFrom.Location = new System.Drawing.Point(277, 95);
+            this.tBoxFrom.Location = new System.Drawing.Point(112, 26);
             this.tBoxFrom.Name = "tBoxFrom";
-            this.tBoxFrom.Size = new System.Drawing.Size(100, 20);
+            this.tBoxFrom.Size = new System.Drawing.Size(445, 20);
             this.tBoxFrom.TabIndex = 1;
             // 
             // tBoxPassword
             // 
             this.tBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tBoxPassword.Location = new System.Drawing.Point(277, 134);
+            this.tBoxPassword.Location = new System.Drawing.Point(112, 65);
             this.tBoxPassword.Name = "tBoxPassword";
-            this.tBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.tBoxPassword.Size = new System.Drawing.Size(445, 20);
             this.tBoxPassword.TabIndex = 2;
             this.tBoxPassword.UseSystemPasswordChar = true;
             // 
             // lbFrom
             // 
             this.lbFrom.AutoSize = true;
-            this.lbFrom.Location = new System.Drawing.Point(172, 98);
+            this.lbFrom.Location = new System.Drawing.Point(24, 26);
             this.lbFrom.Name = "lbFrom";
             this.lbFrom.Size = new System.Drawing.Size(30, 13);
             this.lbFrom.TabIndex = 3;
@@ -76,7 +82,7 @@ namespace Mail
             // lBPassword
             // 
             this.lBPassword.AutoSize = true;
-            this.lBPassword.Location = new System.Drawing.Point(175, 140);
+            this.lBPassword.Location = new System.Drawing.Point(24, 68);
             this.lBPassword.Name = "lBPassword";
             this.lBPassword.Size = new System.Drawing.Size(53, 13);
             this.lBPassword.TabIndex = 4;
@@ -85,7 +91,7 @@ namespace Mail
             // lBTo
             // 
             this.lBTo.AutoSize = true;
-            this.lBTo.Location = new System.Drawing.Point(178, 180);
+            this.lBTo.Location = new System.Drawing.Point(24, 108);
             this.lBTo.Name = "lBTo";
             this.lBTo.Size = new System.Drawing.Size(20, 13);
             this.lBTo.TabIndex = 5;
@@ -93,16 +99,74 @@ namespace Mail
             // 
             // tBoxTo
             // 
-            this.tBoxTo.Location = new System.Drawing.Point(277, 180);
+            this.tBoxTo.Location = new System.Drawing.Point(112, 105);
             this.tBoxTo.Name = "tBoxTo";
-            this.tBoxTo.Size = new System.Drawing.Size(100, 20);
+            this.tBoxTo.Size = new System.Drawing.Size(445, 20);
             this.tBoxTo.TabIndex = 6;
+            // 
+            // lbSubject
+            // 
+            this.lbSubject.AutoSize = true;
+            this.lbSubject.Location = new System.Drawing.Point(24, 147);
+            this.lbSubject.Name = "lbSubject";
+            this.lbSubject.Size = new System.Drawing.Size(43, 13);
+            this.lbSubject.TabIndex = 8;
+            this.lbSubject.Text = "Subject";
+            this.lbSubject.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tBoxSubject
+            // 
+            this.tBoxSubject.Location = new System.Drawing.Point(112, 144);
+            this.tBoxSubject.Name = "tBoxSubject";
+            this.tBoxSubject.Size = new System.Drawing.Size(445, 20);
+            this.tBoxSubject.TabIndex = 9;
+            // 
+            // lbBody
+            // 
+            this.lbBody.AutoSize = true;
+            this.lbBody.Location = new System.Drawing.Point(24, 188);
+            this.lbBody.Name = "lbBody";
+            this.lbBody.Size = new System.Drawing.Size(31, 13);
+            this.lbBody.TabIndex = 10;
+            this.lbBody.Text = "Body";
+            // 
+            // tBoxBody
+            // 
+            this.tBoxBody.Location = new System.Drawing.Point(112, 185);
+            this.tBoxBody.Multiline = true;
+            this.tBoxBody.Name = "tBoxBody";
+            this.tBoxBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tBoxBody.Size = new System.Drawing.Size(445, 138);
+            this.tBoxBody.TabIndex = 11;
+            // 
+            // btAddFile
+            // 
+            this.btAddFile.Location = new System.Drawing.Point(24, 336);
+            this.btAddFile.Name = "btAddFile";
+            this.btAddFile.Size = new System.Drawing.Size(75, 23);
+            this.btAddFile.TabIndex = 12;
+            this.btAddFile.Text = "Add File";
+            this.btAddFile.UseVisualStyleBackColor = true;
+            this.btAddFile.Click += new System.EventHandler(this.btAddFile_Click);
+            // 
+            // tBoxAttachment
+            // 
+            this.tBoxAttachment.Location = new System.Drawing.Point(112, 338);
+            this.tBoxAttachment.Name = "tBoxAttachment";
+            this.tBoxAttachment.Size = new System.Drawing.Size(445, 20);
+            this.tBoxAttachment.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(629, 435);
+            this.Controls.Add(this.tBoxAttachment);
+            this.Controls.Add(this.btAddFile);
+            this.Controls.Add(this.tBoxBody);
+            this.Controls.Add(this.lbBody);
+            this.Controls.Add(this.tBoxSubject);
+            this.Controls.Add(this.lbSubject);
             this.Controls.Add(this.tBoxTo);
             this.Controls.Add(this.lBTo);
             this.Controls.Add(this.lBPassword);
@@ -111,7 +175,7 @@ namespace Mail
             this.Controls.Add(this.tBoxFrom);
             this.Controls.Add(this.btnSendMail);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Send E-Mail";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,6 +190,12 @@ namespace Mail
         private System.Windows.Forms.Label lBPassword;
         private System.Windows.Forms.Label lBTo;
         private System.Windows.Forms.TextBox tBoxTo;
+        private System.Windows.Forms.Label lbSubject;
+        private System.Windows.Forms.TextBox tBoxSubject;
+        private System.Windows.Forms.Label lbBody;
+        private System.Windows.Forms.TextBox tBoxBody;
+        private System.Windows.Forms.Button btAddFile;
+        private System.Windows.Forms.TextBox tBoxAttachment;
     }
 }
 
--- Mail/Form1.cs
+++ Mail/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,15 +21,35 @@ namespace Mail
         private string _server;
         private string _password;
         private int _port;
+        private string _subject;
+        private string _body;
+        private List<Attachment> _attachments;
 
         public Form1()
         {
-            InitializeComponent();          
+            InitializeComponent();
+
+            _attachments = new List<Attachment>();
 
             ReadConfigurationFile();
-            ReadInputData();
-        
-       }
+            LoadDefaultEmail();
+        }
+
+        private void LoadDefaultEmail()
+        {
+
+            tBoxSubject.Text = "Default E-Mail";
+
+            tBoxBody.Text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt" +
+                " ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo " +
+                " dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit " +
+                "  amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt " +
+                "ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores" +
+                " et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem " +
+                "ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et " +
+                "dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet" +
+                " clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+        }
 
         private void ReadConfigurationFile()
         {
@@ -58,42 +79,73 @@ namespace Mail
                     }    
                 }
             }
+
+            WriteKonfigurationIntoInputData();
+        }
+
+        private void WriteKonfigurationIntoInputData()
+        {
+            tBoxFrom.Text = _from;
+            tBoxPassword.Text = _password;
         }
 
         private void ReadInputData()
         {
             _to = tBoxTo.Text;
             _from = tBoxFrom.Text;            
-            _password = tBoxPassword.Text;           
+            _password = tBoxPassword.Text;
+            _subject = tBoxSubject.Text;
+            _body = tBoxBody.Text + GetFooter();
+        }
+
+        private string GetFooter()
+        {
+            // Wird im Mail nicht schön dargestellt:
+            //
+            //string s = "\n";
+            //string s2 = "***** created form JK with C# *****";
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    s += "\n";
+            //    if (i % 2 == 0)
+            //    {
+            //        for (int j = 0; j < s2.Length; j++)
+            //        {
+            //            s += "=";
+            //        }
+            //    }
+            //    else
+            //    {
+            //        s += s2;
+            //    }
+            //}
+            //return s;
+
+            string s = "\n\n***** created form JK with C# *****";
+            return s;
         }
 
         private void btnSendMail_Click(object sender, EventArgs e)
-        {
-
-
-            ReadInputData();           
+        {                
             SendEMail();
-
         }
 
 
         public void SendEMail()
         {
+            ReadInputData();
 
             MailMessage message = new MailMessage(_from, _to);
-            message.Subject = "Using the new SMTP client.";
+            message.Subject = _subject;
+            message.Body = _body;
 
-            message.Body = @"Using this new feature, you can send an email message from an application very easily.";
+            AddAttachmentToMail(ref message);
+
             SmtpClient client = new SmtpClient(_server);
-
-
             client.Port = _port;
             client.Credentials = new NetworkCredential(_from, _password);
             client.EnableSsl = true;
-
-
-            //client.UseDefaultCredentials = true;
-
 
             try
             {
@@ -106,8 +158,67 @@ namespace Mail
             }
         }
 
-       
+        private void AddAttachmentToMail( ref MailMessage message)
+        {
+
+            foreach (var attachement in _attachments)
+            {
+                message.Attachments.Add(attachement);
+                
+            }     
+        }
+
+        private void LoadAttachment(string path , string name)
+        {
+
+            string file = @""+ path;
+            // Create  the file attachment for this email message.
+            Attachment data = new Attachment(file, MediaTypeNames.Application.Octet);
+            // Add time stamp information for the file.
+            //ContentDisposition disposition = data.ContentDisposition;
+            //disposition.CreationDate = System.IO.File.GetCreationTime(file);
+            //disposition.ModificationDate = System.IO.File.GetLastWriteTime(file);
+            //disposition.ReadDate = System.IO.File.GetLastAccessTime(file);
+            // Add the file attachment to this email message.
+           
 
 
+            _attachments.Add(data);
+
+            ViewAttachments();
+        }
+
+
+
+        private void btTakeFrom_Click(object sender, EventArgs e)
+        {
+            tBoxTo.Text = tBoxFrom.Text;
+        }
+
+        private void btAddFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+                string fileName = openFileDialog.SafeFileName;
+                LoadAttachment(path, fileName);
+
+               
+            } 
+        }
+
+        private void ViewAttachments()
+        {
+
+            string[] fileNames = _attachments.Select(x => x.Name).ToArray();
+            string attachmentString = "";
+            foreach (var fileName in fileNames)
+            {
+                attachmentString += fileName + "; ";
+
+            }
+            tBoxAttachment.Text = attachmentString;
+        }
     }
 }
